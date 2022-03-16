89c71f7362199635303362c3cb5d92e6b8f33bdc
Isox-z <theo.meunier41@gmail.com>
2022-03-16 12:53:42 +0100

    fix: ERROR 500 and migrate webpack encore

---
+++ assets/app.js
@@ -0,0 +1,8 @@
+// start the Stimulus application
+import './bootstrap';
+
+import './scss/style.scss'
+import './js/lightbox.js'
+import './js/ckeditor.js'
+import './js/modal_discographie.js'
+
--- assets/main.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import '/scss/style.scss'
-import '/js/lightbox.js'
-import '/js/ckeditor.js'
-import '/js/modal_discographie.js'
-
